f01726ff5244c4ee47167846db8023954ad9c414
Bruno H. Homem <brunohhomem@gmail.com>
2024-03-08 18:04:39 -0300

    prev & next buttons

--- js/script.js
+++ js/script.js
@@ -7,10 +7,17 @@ const pokemonImage = document.querySelector('.pokemon__image')
 const form = document.querySelector('.form')
 const input = document.querySelector('.input__search')
 
+//Variaveis dos botoes
+const buttonPrev = document.querySelector('.btn-prev')
+const buttonNext = document.querySelector('.btn-next')
+
+//variavel para auxiliar na contagem dos pokemons para os botoes de prev e next
+let searchPokemon = 1
+
 //função que busca os pokemons no servidor, faz o get da api
 const fetchPokemon = async pokemon => {
   const APIResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   )
 
   if (APIResponse.status === 200) {
@@ -28,8 +35,10 @@ const renderPokemon = async pokemon => {
 
   if (data) {
     //so vai mostrar os dados caso o data tenha um pokemon
+    pokemonImage.style.display = 'block'
     pokemonName.innerHTML = data.name //retorna nome
     pokemonNumber.innerHTML = data.id //retorna id
+    searchPokemon = data.id //faz a variavel pegar a id que pesquisamos
     pokemonImage.src =
       data['sprites']['versions']['generation-v']['black-white']['animated'][
         'front_default'
@@ -39,14 +48,26 @@ const renderPokemon = async pokemon => {
   } else {
     pokemonName.innerHTML = 'Not found :('
     pokemonNumber.innerHTML = ''
-    pokemonImage.src = null
+    pokemonImage.style.display = 'none'
   }
 }
 
 form.addEventListener('submit', event => {
   event.preventDefault()
 
-  renderPokemon(input.value)
+  renderPokemon(input.value.toLowerCase())
 })
 
-renderPokemon('1')
+buttonPrev.addEventListener('click', () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1
+    renderPokemon(searchPokemon)
+  }
+})
+
+buttonNext.addEventListener('click', () => {
+  searchPokemon += 1
+  renderPokemon(searchPokemon)
+})
+
+renderPokemon(searchPokemon)
