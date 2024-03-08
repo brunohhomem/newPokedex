3b27777a7f317e7d1f0d593b095010f6a68ab3e4
Bruno H. Homem <brunohhomem@gmail.com>
2024-03-08 17:51:39 -0300

    fuction to search pokes by name or id

--- js/script.js
+++ js/script.js
@@ -3,28 +3,50 @@ const pokemonName = document.querySelector('.pokemon__name')
 const pokemonNumber = document.querySelector('.pokemon__number')
 const pokemonImage = document.querySelector('.pokemon__image')
 
-//função que busca os pokemons no servidor, faz o get da api
+//Variaveis da busca
+const form = document.querySelector('.form')
+const input = document.querySelector('.input__search')
 
+//função que busca os pokemons no servidor, faz o get da api
 const fetchPokemon = async pokemon => {
   const APIResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    `https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`
   )
 
-  const data = await APIResponse.json()
-  return data
+  if (APIResponse.status === 200) {
+    //so vai retornar se encontrar o pokemon
+    const data = await APIResponse.json()
+    return data
+  }
 }
 
 //Função que renderiza o pokemon na tela
-
 const renderPokemon = async pokemon => {
+  pokemonName.innerHTML = 'Loading...'
+
   const data = await fetchPokemon(pokemon)
 
-  pokemonName.innerHTML = data.name //retorna nome
-  pokemonNumber.innerHTML = data.id //retorna id
-  pokemonImage.src =
-    data['sprites']['versions']['generation-v']['black-white']['animated'][
-      'front_default'
-    ] //retorna sprite
+  if (data) {
+    //so vai mostrar os dados caso o data tenha um pokemon
+    pokemonName.innerHTML = data.name //retorna nome
+    pokemonNumber.innerHTML = data.id //retorna id
+    pokemonImage.src =
+      data['sprites']['versions']['generation-v']['black-white']['animated'][
+        'front_default'
+      ] //retorna sprite
+
+    input.value = ''
+  } else {
+    pokemonName.innerHTML = 'Not found :('
+    pokemonNumber.innerHTML = ''
+    pokemonImage.src = null
+  }
 }
 
-renderPokemon('123')
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  renderPokemon(input.value)
+})
+
+renderPokemon('1')
